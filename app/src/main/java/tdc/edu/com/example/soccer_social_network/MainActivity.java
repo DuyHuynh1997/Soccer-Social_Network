462bb95672da860d152f4b4a3c3462a48d94cfa1
DuyHuynh1997 <duyhuynhss97@gmail.com>
2020-07-24 08:06:46 +0700

    Initial commit

---
+++ app/src/main/java/tdc/edu/com/example/soccer_social_network/MainActivity.java
@@ -0,0 +1,14 @@
+package tdc.edu.com.example.soccer_social_network;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
