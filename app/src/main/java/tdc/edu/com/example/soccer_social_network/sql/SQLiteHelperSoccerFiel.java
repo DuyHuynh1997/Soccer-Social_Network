22225ffa3fae153fa2b31ac6c70651703375faf7
TranAnhKhiem <44549170+TranAnhKhiem@users.noreply.github.com>
2020-07-29 23:58:33 +0700

    Sửa đổi layout row_listview, Thêm chức năng thêm và xem chi tiết với SQLite

---
+++ app/src/main/java/tdc/edu/com/example/soccer_social_network/sql/SQLiteHelperSoccerFiel.java
@@ -0,0 +1,81 @@
+package tdc.edu.com.example.soccer_social_network.sql;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteStatement;
+
+public class SQLiteHelperSoccerFiel extends SQLiteOpenHelper {
+    private static String DB_NAME = "datasan";
+    private static int DB_VERSION = 1;
+
+
+
+    public SQLiteHelperSoccerFiel(Context context) {
+        super( context, DB_NAME, null, DB_VERSION );
+    }
+
+    public void queryData(String sql) {
+        SQLiteDatabase database = getWritableDatabase();
+        database.execSQL( sql );
+    }
+
+    public void insertData(String name, String area, byte[] image) {
+        SQLiteDatabase database = getWritableDatabase();
+        String sql = "INSERT INTO SOCCERFIELD VALUES (NULL, ?, ?, ?)";
+
+        SQLiteStatement statement = database.compileStatement(sql);
+        statement.clearBindings();
+
+        statement.bindString( 1, name);
+        statement.bindString( 2, area);
+        statement.bindBlob( 3, image);
+
+        statement.executeInsert();
+    }
+
+    public void updateData(String name, String area, byte[] image, int id) {
+        SQLiteDatabase database = getWritableDatabase();
+
+        String sql = "UPDATE SOCCERFIELD SET name = ?, area = ?, image = ? WHERE id = ?";
+        SQLiteStatement statement = database.compileStatement(sql);
+
+        statement.bindString(1, name);
+        statement.bindString(2, area);
+        statement.bindBlob(3, image);
+        statement.bindDouble(4, (double)id);
+
+        statement.execute();
+        database.close();
+    }
+
+    public  void deleteData(int id) {
+        SQLiteDatabase database = getWritableDatabase();
+
+        String sql = "DELETE FROM SOCCERFIELD WHERE id = ?";
+        SQLiteStatement statement = database.compileStatement(sql);
+        statement.clearBindings();
+        statement.bindDouble(1, (double)id);
+
+        statement.execute();
+        database.close();
+    }
+
+    public Cursor getData(String sql)
+    {
+        SQLiteDatabase database = getReadableDatabase();
+        return database.rawQuery( sql,null );
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
