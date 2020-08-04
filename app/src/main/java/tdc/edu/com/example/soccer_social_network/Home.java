a9a963baf50c84f4bcabe7c1bacfb5b19c48e139
leminhmy <minhmy004@gmail.com>
2020-08-04 19:34:52 +0700

    aa

---
+++ app/src/main/java/tdc/edu/com/example/soccer_social_network/Home.java
@@ -0,0 +1,20 @@
+package tdc.edu.com.example.soccer_social_network;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Home extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.home_fragment, container, false);
+    }
+}
